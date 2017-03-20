07a0967e2dc58f3835f28362a87786cb738e3e56
Valentin1DAM <titi_malvinas@hotmail.com>
2017-03-20 01:37:09 +0100

    Primera version (dia 20 / 1:24)

--- src/valentin1dam/Valentin1DAM.java
+++ src/valentin1dam/Valentin1DAM.java
@@ -18,7 +18,7 @@ public class Valentin1DAM {
         System.out.println("Inicio de proyecto");
         System.out.println("Inicio de proyecto");
          System.out.println("Inicio de proyecto");
-         System.our.println("Version del dia 20 de marzo de 2017");
+         System.out.println("Version del dia 20 de marzo de 2017");
     }
     
 }
